95f8da23df7da767d108deee5d523d3832a602d5
方辉 <773613033@qq.com>
2017-10-07 14:51:17 +0800

    Rejected commit: Default

---
+++ dht-root/dht-manage/src/main/java/com/retailers/dht/manage/controller/GoodsBrandController.java
@@ -0,0 +1,83 @@
+package com.retailers.dht.manage.controller;
+
+import com.retailers.auth.annotation.CheckSession;
+import com.retailers.auth.annotation.Function;
+import com.retailers.auth.annotation.Menu;
+import com.retailers.auth.constant.SystemConstant;
+import com.retailers.dht.common.entity.GoodsBrand;
+import com.retailers.dht.common.service.GoodsBrandService;
+import com.retailers.dht.common.service.GoodsBrandService;
+import com.retailers.dht.manage.base.BaseController;
+import com.retailers.mybatis.pagination.Pagination;
+import com.retailers.tools.base.BaseResp;
+import com.retailers.tools.utils.PageUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Administrator on 2017/9/28.
+ */
+@Controller
+@RequestMapping("goods")
+public class GoodsBrandController extends BaseController {
+
+    @Autowired
+    GoodsBrandService goodsBrandService;
+
+    @RequestMapping("/openGoodsBrand")
+    @Menu(parentRes = "sys.manager.goods",resourse = "goods.openGoodsBrand",description = "商品品牌管理",sort = 1,label = "商品品牌管理")
+    public String openGoodsBrand(){
+        return "goods/goodsBrand";
+    }
+
+    @RequestMapping("editGoodsBrand")
+    @Function(label = "编辑商品品牌",parentRes = "goods.openGoodsBrand",resourse = "goods.editGoodsBrand",description = "编辑商品品牌",sort = 2)
+    @CheckSession(key= SystemConstant.LOG_USER_SESSION_KEY,msg = "未登陆，请重新登录",redirect = "http://www.baidu.com")
+    @ResponseBody
+    public BaseResp editGoodsBrand(GoodsBrand GoodsBrand){
+        boolean flag = goodsBrandService.updateGoodsBrand(GoodsBrand);
+        if(flag){
+            return success("修改商品品牌["+GoodsBrand.getGbName()+"]成功");
+        }else{
+            return errorForSystem("修改商品品牌["+GoodsBrand.getGbName()+"]失败");
+        }
+    }
+
+    @RequestMapping("/removeGoodsBrand")
+    @Function(label="删除商品品牌", description = "删除商品品牌", resourse = "goods.removeGoodsBrand",sort=3,parentRes="goods.openGoodsBrand")
+    @CheckSession(key = SystemConstant.LOG_USER_SESSION_KEY,msg="未登陆，请重新登录",redirect = "http://www.baidu.com")
+    @ResponseBody
+    public BaseResp removeGoodsBrand(Long gbId){
+        boolean flag=goodsBrandService.deleteGoodsBrandByGbId(gbId);
+        return success(flag);
+    }
+
+    @RequestMapping("/queryGoodsBrandLists")
+    @Function(label="商品品牌列表", description = "所有商品品牌列表", resourse = "goods.queryGoodsBrandLists",sort=1,parentRes="goods.openGoodsBrand")
+    @CheckSession(key = SystemConstant.LOG_USER_SESSION_KEY,msg="未登陆，请重新登录",redirect = "http://www.baidu.com")
+    @ResponseBody
+    public  Map<String,Object> queryGoodsBrandLists(String gbName,PageUtils pageForm){
+        Map<String,Object> map = new HashMap<String,Object>();
+        map.put("gbName",gbName);
+        Pagination<GoodsBrand> GoodsBrandPagination = goodsBrandService.queryGoodsBrandList(map,pageForm.getPageNo(),pageForm.getPageSize());
+        Map<String,Object> gtm = new HashMap<String,Object>();
+        gtm.put("total",GoodsBrandPagination.getTotalCount());
+        gtm.put("rows",GoodsBrandPagination.getData());
+        return gtm;
+    }
+
+    @RequestMapping("/addGoodsBrand")
+    @Function(label="增加商品品牌", description = "增加商品品牌", resourse = "goods.addGoodsBrand",parentRes="goods.openGoodsBrand")
+    @CheckSession(key = SystemConstant.LOG_USER_SESSION_KEY,msg="未登陆，请重新登录",redirect = "http://www.baidu.com")
+    @ResponseBody
+    public BaseResp addGoodsBrand(GoodsBrand GoodsBrand){
+        boolean flag=goodsBrandService.saveGoodsBrand(GoodsBrand);
+        return success(flag);
+    }
+
+}
---
+++ dht-root/dht-manage/src/main/java/com/retailers/dht/manage/controller/GoodsTypeController.java
@@ -0,0 +1,82 @@
+package com.retailers.dht.manage.controller;
+
+import com.retailers.auth.annotation.CheckSession;
+import com.retailers.auth.annotation.Function;
+import com.retailers.auth.annotation.Menu;
+import com.retailers.auth.constant.SystemConstant;
+import com.retailers.dht.common.entity.GoodsType;
+import com.retailers.dht.common.service.GoodsTypeService;
+import com.retailers.dht.manage.base.BaseController;
+import com.retailers.mybatis.pagination.Pagination;
+import com.retailers.tools.base.BaseResp;
+import com.retailers.tools.utils.PageUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Administrator on 2017/9/28.
+ */
+@Controller
+@RequestMapping("goods")
+public class GoodsTypeController extends BaseController {
+
+    @Autowired
+    GoodsTypeService goodsTypeService;
+
+    @RequestMapping("/openGoodsType")
+    @Menu(parentRes = "sys.manager.goods",resourse = "goods.openGoodsType",description = "商品大类管理",sort = 1,label = "商品大类管理")
+    public String openGoodsType(){
+        return "goods/goodsType";
+    }
+
+    @RequestMapping("editGoodsType")
+    @Function(label = "编辑商品大类",parentRes = "goods.openGoodsType",resourse = "goods.editGoodstype",description = "编辑商品大类",sort = 2)
+    @CheckSession(key= SystemConstant.LOG_USER_SESSION_KEY,msg = "未登陆，请重新登录",redirect = "http://www.baidu.com")
+    @ResponseBody
+    public BaseResp editGoodsType(GoodsType goodsType){
+        boolean flag = goodsTypeService.updateGoodsType(goodsType);
+        if(flag){
+            return success("修改商品大类["+goodsType.getGtName()+"]成功");
+        }else{
+            return errorForSystem("修改商品大类["+goodsType.getGtName()+"]失败");
+        }
+    }
+
+    @RequestMapping("/removeGoodsType")
+    @Function(label="删除商品大类", description = "删除商品大类", resourse = "goods.removeGoodstype",sort=3,parentRes="goods.openGoodsType")
+    @CheckSession(key = SystemConstant.LOG_USER_SESSION_KEY,msg="未登陆，请重新登录",redirect = "http://www.baidu.com")
+    @ResponseBody
+    public BaseResp removeGoodsType(Long gtId){
+        boolean flag=goodsTypeService.deleteGoodsTypeByGtId(gtId);
+        return success(flag);
+    }
+
+    @RequestMapping("/queryGoodsTypeLists")
+    @Function(label="商品大类列表", description = "所有商品大类列表", resourse = "goods.queryGoodsTypeLists",sort=1,parentRes="goods.openGoodsType")
+    @CheckSession(key = SystemConstant.LOG_USER_SESSION_KEY,msg="未登陆，请重新登录",redirect = "http://www.baidu.com")
+    @ResponseBody
+    public  Map<String,Object> queryGoodsTypeLists(String gtName,PageUtils pageForm){
+        Map<String,Object> map = new HashMap<String,Object>();
+        map.put("gtName",gtName);
+        Pagination<GoodsType> goodsTypePagination = goodsTypeService.queryGoodsTypeList(map,pageForm.getPageNo(),pageForm.getPageSize());
+        Map<String,Object> gtm = new HashMap<String,Object>();
+        gtm.put("total",goodsTypePagination.getTotalCount());
+        gtm.put("rows",goodsTypePagination.getData());
+        return gtm;
+    }
+
+    @RequestMapping("/addGoodsType")
+    @Function(label="增加商品大类", description = "增加商品大类", resourse = "goods.addGoodsType",parentRes="goods.openGoodsType")
+    @CheckSession(key = SystemConstant.LOG_USER_SESSION_KEY,msg="未登陆，请重新登录",redirect = "http://www.baidu.com")
+    @ResponseBody
+    public BaseResp addGoodsType(GoodsType goodsType){
+        boolean flag=goodsTypeService.saveGoodsType(goodsType);
+        return success(flag);
+    }
+
+}
