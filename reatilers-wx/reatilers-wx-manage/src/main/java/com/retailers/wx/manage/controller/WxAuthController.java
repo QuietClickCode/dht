5f42915da0d05d5cfed4523ce7aecdf9a3370220
zpaman <zpaman@126.com>
2017-11-03 17:37:25 +0800

    微信登陆

--- reatilers-wx/reatilers-wx-manage/src/main/java/com/retailers/wx/manage/controller/WxAuthController.java
+++ reatilers-wx/reatilers-wx-manage/src/main/java/com/retailers/wx/manage/controller/WxAuthController.java
@@ -1,16 +1,20 @@
 package com.retailers.wx.manage.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import com.retailers.tools.utils.HttpClientUtil;
 import com.retailers.tools.utils.ObjectUtils;
-import com.retailers.tools.utils.StringUtils;
 import com.retailers.wx.common.config.WxConfig;
+import com.sun.tools.javac.code.Source;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,13 +29,25 @@ public class WxAuthController {
     Logger logger = LoggerFactory.getLogger(WxAuthController.class);
 
     /**
+     * 获得用户授权确认
+     */
+    @RequestMapping("queryUserAuth")
+    public void queryUserAuth(HttpServletResponse response) throws IOException {
+        String url="https://open.weixin.qq.com/connect/oauth2/authorize?appid="+WxConfig.APP_ID+"&redirect_uri="+ URLEncoder.encode("http://zpaman.free.ngrok.cc/wxAuth/userLogin","utf-8")+"&response_type=code&scope=SCOPE&state=STATE#wechat_redirect";
+        response.sendRedirect(url);
+    }
+
+
+    /**
      * 路由控制
      *
      * @param request
-     * @param code
      * @return
      */
+    @RequestMapping("userLogin")
     public void auth(HttpServletRequest request,String code) {
+        System.out.println("------------------------------------------------->:"+code);
+//        String code =WxConfig.ACCESS_TOKEN;
         Map<String, String> data = new HashMap();
         Map<String, String> result = getUserInfoAccessToken(code);//通过这个code获取access_token
         String openId = result.get("openid");
@@ -54,22 +70,18 @@ public class WxAuthController {
     public Map<String, String> getUserInfoAccessToken(String code) {
         JSONObject object = null;
         Map<String, String> data = new HashMap();
-//        try {
-//            String url = String.format("https://api.weixin.qq.com/sns/oauth2/access_token?appid=%s&secret=%s&code=%s&grant_type=authorization_code",
-//                    WxConfig.APP_ID,WxConfig.APP_SECRET, code);
-//            logger.info("request accessToken from url: {}", url);
-//            DefaultHttpClient httpClient = new DefaultHttpClient();
-//            HttpGet httpGet = new HttpGet(url);
-//            HttpResponse httpResponse = httpClient.execute(httpGet);
-//            HttpEntity httpEntity = httpResponse.getEntity();
-//            String tokens = EntityUtils.toString(httpEntity, "utf-8");
-//            object = JSONObject.parseObject(tokens);
-//            logger.info("request accessToken success. [result={}]", object);
-//            data.put("openid", object.get("openid").toString().replaceAll("\"", ""));
-//            data.put("access_token", object.get("access_token").toString().replaceAll("\"", ""));
-//        } catch (Exception ex) {
-//            logger.error("fail to request wechat access token. [error={}]", ex);
-//        }
+        try {
+            String url = String.format("https://api.weixin.qq.com/sns/oauth2/access_token?appid=%s&secret=%s&code=%s&grant_type=authorization_code",
+                    WxConfig.APP_ID,WxConfig.APP_SECRET, code);
+            logger.info("request accessToken from url: {}", url);
+            String rtn = HttpClientUtil.doGet(url);
+            object = JSONObject.parseObject(rtn);
+            logger.info("request accessToken success. [result={}]", object);
+            data.put("openid", object.get("openid").toString().replaceAll("\"", ""));
+            data.put("access_token", object.get("access_token").toString().replaceAll("\"", ""));
+        } catch (Exception ex) {
+            logger.error("fail to request wechat access token. [error={}]", ex);
+        }
         return data;
     }
 
@@ -83,27 +95,22 @@ public class WxAuthController {
      */
     public Map<String, String> getUserInfo(String accessToken, String openId) {
         Map<String, String> data = new HashMap();
-//        String url = "https://api.weixin.qq.com/sns/userinfo?access_token=" + accessToken + "&openid=" + openId + "&lang=zh_CN";
-//        logger.info("request user info from url: {}", url);
-//        JsonObject userInfo = null;
-//        try {
-//            DefaultHttpClient httpClient = new DefaultHttpClient();
-//            HttpGet httpGet = new HttpGet(url);
-//            HttpResponse httpResponse = httpClient.execute(httpGet);
-//            HttpEntity httpEntity = httpResponse.getEntity();
-//            String response = EntityUtils.toString(httpEntity, "utf-8");
-//            Gson token_gson = new Gson();
-//            userInfo = token_gson.fromJson(response, JsonObject.class);
-//            logger.info("get userinfo success. [result={}]", userInfo);
-//            data.put("openid", userInfo.get("openid").toString().replaceAll("\"", ""));
-//            data.put("nickname", userInfo.get("nickname").toString().replaceAll("\"", ""));
-//            data.put("city", userInfo.get("city").toString().replaceAll("\"", ""));
-//            data.put("province", userInfo.get("province").toString().replaceAll("\"", ""));
-//            data.put("country", userInfo.get("country").toString().replaceAll("\"", ""));
-//            data.put("headimgurl", userInfo.get("headimgurl").toString().replaceAll("\"", ""));
-//        } catch (Exception ex) {
-//            logger.error("fail to request wechat user info. [error={}]", ex);
-//        }
+        String url = "https://api.weixin.qq.com/sns/userinfo?access_token=" + accessToken + "&openid=" + openId + "&lang=zh_CN";
+        logger.info("request user info from url: {}", url);
+        try {
+            String info = HttpClientUtil.doGet(url);;
+            JSONObject userInfo=JSONObject.parseObject(info);
+            logger.info("get userinfo success. [result={}]", info);
+            System.out.println(info);
+            data.put("openid", userInfo.get("openid").toString().replaceAll("\"", ""));
+            data.put("nickname", userInfo.get("nickname").toString().replaceAll("\"", ""));
+            data.put("city", userInfo.get("city").toString().replaceAll("\"", ""));
+            data.put("province", userInfo.get("province").toString().replaceAll("\"", ""));
+            data.put("country", userInfo.get("country").toString().replaceAll("\"", ""));
+            data.put("headimgurl", userInfo.get("headimgurl").toString().replaceAll("\"", ""));
+        } catch (Exception ex) {
+            logger.error("fail to request wechat user info. [error={}]", ex);
+        }
         return data;
     }
 }
