820488ee7b93c4beba30e320e67d56a43155a21b
zpaman <zpaman@126.com>
2017-11-22 11:23:54 +0800

    改变菜单 结构

--- dht-root/dht-manage/src/main/java/com/retailers/dht/manage/controller/BaseMenuController.java
+++ dht-root/dht-manage/src/main/java/com/retailers/dht/manage/controller/BaseMenuController.java
@@ -8,9 +8,9 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class BaseMenuController {
-    @Resourse(resourse = "sys.manager.permissions",label = "系统设置")
+    @Resourse(resourse = "sys.manager.permissions",label = "系统设置",sort = 0)
     public String permissions;
-    @Resourse(resourse = "sys.manager.goods",label = "商品管理")
+    @Resourse(resourse = "sys.manager.goods",label = "商品管理",sort = 2)
     public String goodsManaage;
     @Resourse(resourse="sys.manager.org",label="部门管理",parentRes = "sys.manager.permissions",sort = 2)
     public String menu2;
@@ -18,31 +18,9 @@ public class BaseMenuController {
     public String userManange;
     @Resourse(resourse="sys.menu.manager",label="资源管理",parentRes = "sys.manager.permissions",sort = 1)
     public String menuManage;
-    @Resourse(resourse="sys.menu.test",label="测试菜单")
-    public String menu6;
-    @Resourse(resourse="sys.menu.test1",label="测试菜单1",parentRes="sys.menu.test")
-    public String menu7;
-
-    @Resourse(resourse="sys.menu.test2",label="测试菜单2",parentRes="sys.menu.test1")
-    public String menu8;
-    @Resourse(resourse="sys.menu.test3",label="测试菜单3",parentRes="sys.menu.test2")
-    public String menu9;
-    @Resourse(resourse="sys.menu.test4",label="测试菜单4",parentRes="sys.menu.test3")
-    public String menu10;
-    @Resourse(resourse="sys.menu.test5",label="测试菜单5",parentRes="sys.menu.test4")
-    public String menu11;
-    @Resourse(resourse="sys.menu.test6",label="测试菜单6",parentRes="sys.menu.test5")
-    public String menu12;
-    @Resourse(resourse="sys.menu.test7",label="测试菜单7",parentRes="sys.menu.test6")
-    public String menu13;
-    @Resourse(resourse="sys.menu.test8",label="测试菜单8",parentRes="sys.menu.test7")
-    public String menu14;
-    @Resourse(resourse="sys.menu.test9",label="测试菜单9",parentRes="sys.menu.test7")
-    public String menu15;
-
-    @Resourse(resourse = "sys.manager.promotion",label = "促销方式",sort =2)
+    @Resourse(resourse = "sys.manager.promotion",label = "促销方式",sort =3)
     public String promotion;
-    @Resourse(resourse = "sys.manager.customer",label = "会员管理",sort = 3)
+    @Resourse(resourse = "sys.manager.customer",label = "会员管理",sort = 1)
     public String customer;
 
     @Resourse(resourse = "sys.manager.floorManage",label = "导航/广告设置",sort = 5)
